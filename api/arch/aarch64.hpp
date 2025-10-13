9402db9a67b1cadc69c498ef38a1244842f89275
e512c <e512c.dev@outlook.com>
2025-10-13 12:28:17 +0200

    aarch64 cpu_relax() support

--- api/arch/aarch64.hpp
+++ api/arch/aarch64.hpp
@@ -34,14 +34,19 @@ namespace os {
     static constexpr uintptr_t   min_page_size      = 4096;
     static constexpr const char* name               = "aarch64";
     static inline uint64_t cpu_cycles() noexcept;
+    static inline void cpu_relax() noexcept;
   };
 }
-//IMPL
+// IMPL
 constexpr uintptr_t __arch_max_canonical_addr = 0x7ffffffffff;
 uint64_t os::Arch::cpu_cycles() noexcept {
   uint64_t ret;
   //isb then read
   asm volatile("isb;mrs %0, pmccntr_el0" : "=r"(ret));
   return ret;
+}
+
+inline void os::Arch::cpu_relax() noexcept {
+  asm volatile ("yield");
 }
 #endif //AARCH64_ARCH_HPP
