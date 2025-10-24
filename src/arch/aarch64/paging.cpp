522162f71fe6c9e41c7ecc95910b6763ebba7897
e512c <e512c.dev@outlook.com>
2025-10-24 14:18:45 +0200

    re-add paging info message

--- src/arch/aarch64/paging.cpp
+++ src/arch/aarch64/paging.cpp
@@ -17,11 +17,12 @@
 
 #include <arch.hpp>
 #include <kernel/memory.hpp>
+#include <info>
 
 __attribute__((weak))
 void __arch_init_paging()
 {
-  /*INFO("aarch64", "Paging not enabled by default on");*/
+  INFO("aarch64", "Paging not enabled by default on");
 }
 
 namespace os {
