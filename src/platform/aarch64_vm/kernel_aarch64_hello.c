85cbc15db5f686de3391940e799ba47a891b5c71
e512c <e512c.dev@outlook.com>
2025-10-31 15:55:42 +0100

    lkljsfd

--- src/platform/aarch64_vm/kernel_aarch64_hello.c
+++ src/platform/aarch64_vm/kernel_aarch64_hello.c
@@ -1,3 +1,9 @@
+// NOTE:
+// just a callable function for a simple print
+// in a c-file, call like this in assembly:
+//
+// bl kernel_aarch64_hello
+
 #include <stdint.h>
 
 volatile uint8_t *uart = (uint8_t *) 0x09000000;
