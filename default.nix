869b10e6770c862bc2e273f8035110598df0cb8c
e512c <e512c.dev@outlook.com>
2025-11-13 17:11:24 +0100

    new default.nix config

--- default.nix
+++ default.nix
@@ -11,17 +11,34 @@
 
     # Target machine (the system for which we are building binaries)
     #
-    # NOTE: remove this for x86 builds.
     crossSystem = {
-      config = "aarch64-unknown-linux-musl";
+      config = "x86_64-unknown-linux-musl";
+      # config = "aarch64-unknown-linux-musl";
     };
   }
 }:
 
-assert (pkgs.pkgsIncludeOS.stdenv.buildPlatform.isLinux == false) ->
-throw "Currently only Linux builds are supported";
+let
+  supportedTargets.system = [
+    "x86_64-linux"
+    "i686-linux"
+    "aarch64-linux"
+  ];
 
-assert (pkgs.pkgsIncludeOS.stdenv.hostPlatform.isMusl == false) ->
-throw "Stdenv should be based on Musl";
+  assertMsg      = pkgs.lib.asserts.assertMsg;
+  assertOneOf    = pkgs.lib.asserts.assertOneOf;
+  buildPlatform  = pkgs.pkgsIncludeOS.stdenv.buildPlatform;
+  compilerEnv    = pkgs.pkgsIncludeOS.stdenv.hostPlatform;
+  targetPlatform = pkgs.pkgsIncludeOS.stdenv.targetPlatform;
+
+in
+
+assert assertMsg buildPlatform.isLinux
+"Currently only Linux builds are supported";
+
+assert assertMsg compilerEnv.isMusl
+"Stdenv should be based on Musl";
+
+assert assertOneOf "crossSystem.system" targetPlatform.system supportedTargets.system;
 
 pkgs.pkgsIncludeOS.includeos
