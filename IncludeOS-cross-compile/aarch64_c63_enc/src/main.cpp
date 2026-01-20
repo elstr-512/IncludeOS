42c13bebca5d60cb94b5278e10b52293aa040a4a
e512c <e512c.dev@outlook.com>
2026-01-20 16:58:53 +0100

    C63 - Embed yuv-video with cmake
    The #embed directive killed my
    LSP performance >:(

--- IncludeOS-cross-compile/aarch64_c63_enc/src/main.cpp
+++ IncludeOS-cross-compile/aarch64_c63_enc/src/main.cpp
@@ -1,14 +1,18 @@
+#include <cstddef>
 #include <os>
 #include <service>
 
 #include <stdio.h>
 
-const unsigned char embedded_video[] = {
-    #embed "foreman.yuv" // Clang extension
-};
+extern "C" {
+  extern const unsigned char __binary_yuv_start[];
+  extern const unsigned char __binary_yuv_end[];
 
-static void dump(const uint8_t arr[], size_t size)
-{
+  const size_t __binary_yuv_size =
+    __binary_yuv_end - __binary_yuv_start;
+}
+
+static void dump(const uint8_t arr[], size_t size) {
     for (
       size_t i = 0;
       i != size && i < (10 * 16);
@@ -23,29 +27,12 @@ static void dump(const uint8_t arr[], size_t size)
 }
 
 static void embedded_test() {
-    printf("Embedded data size: %zu bytes\n", sizeof(embedded_video));
-    printf("data[]:\n");
-    dump((const uint8_t*)embedded_video, sizeof embedded_video);
+  printf("Embedded data size: %zu bytes\n", __binary_yuv_size);
+  dump(__binary_yuv_start, __binary_yuv_size);
 }
 
-static void cpp_version() {
-  switch (__cplusplus) {
-    case 202302L: std::cout << "C++23"; break;
-    case 202002L: std::cout << "C++20"; break;
-    case 201703L: std::cout << "C++17"; break;
-    case 201402L: std::cout << "C++14"; break;
-    case 201103L: std::cout << "C++11"; break;
-    case 199711L: std::cout << "C++98"; break;
-    default:
-    std::cout << "pre-standard C++ or custom standard";
-  }
-  std::cout << " (" << __cplusplus << ")\n";
-}
-
-void Service::start(const std::string& args){
+void Service::start(const std::string& args) {
   printf("Args = %s\n", args.c_str());
-
-  cpp_version();
 
   puts("");
   embedded_test();
