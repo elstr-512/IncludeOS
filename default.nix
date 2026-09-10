9d24ec322ebb580d3bc43952ae4fe2bd65776480
e512c <e512c.dev@outlook.com>
2026-09-10 17:39:47 +0200

    cross-build default: add cross-compilation config

--- default.nix
+++ default.nix
@@ -20,11 +20,31 @@
 }:
 
 let
-  inherit (pkgs) pkgsIncludeOS;
+  # cross compile configured nix packages
+  supportedTargets.pkgs = {
+    x86_64 = import nixpkgs {
+      overlays = overlays;
+      crossSystem = { config = "x86_64-unknown-linux-musl"; };
+    };
+
+    aarch64 = import nixpkgs {
+      overlays = overlays;
+      crossSystem = { config = "aarch64-unknown-linux-musl"; };
+    };
+
+    i686 = import nixpkgs {
+      overlays = overlays;
+      crossSystem = { config = "i686-unknown-linux-musl"; };
+    };
+  };
+
+  # Select pkgs configuration
+  pkgs = supportedTargets.pkgs.${target};
+
 in
   assert (pkgsIncludeOS.stdenv.buildPlatform.isLinux == false) ->
     throw "Currently only Linux builds are supported";
   assert (pkgsIncludeOS.stdenv.hostPlatform.isMusl == false) ->
     throw "Stdenv should be based on Musl";
 
-  pkgsIncludeOS.includeos
+pkgs.pkgsIncludeOS.includeos
