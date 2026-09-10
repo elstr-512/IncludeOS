f00165c6eea1704dfb91b3923d8d334ba32e62ba
e512c <e512c.dev@outlook.com>
2026-09-10 17:37:02 +0200

    cross-build default: add nix args

--- default.nix
+++ default.nix
@@ -1,10 +1,22 @@
-{ withCcache ? false, # Enable ccache. Requires correct permissions, see overlay.nix.
-  smp ? false, # Enable multcore support (SMP)
+{
   nixpkgs ? ./pinned.nix,
+
   overlays ? [
-    (import ./overlay.nix { inherit withCcache; inherit smp; disableTargetWarning = true; } )
+    ( import ./overlay.nix {inherit withCcache smp;} )
   ],
-  pkgs ? import nixpkgs { config = {}; inherit overlays; }
+
+  # Which architecture to build against,
+  # can be passed via command line: --argstr target <string>
+  target ? "x86_64",
+
+  # Enable ccache support. See overlay.nix for details.
+  withCcache ? false,
+
+  # Enable multicore suport (SMP).
+  # WARN:
+  # While the SMP flag exists in the nix-config,
+  # it is currently always defined as ON in CMakelists.txt
+  smp ? false,
 }:
 
 let
